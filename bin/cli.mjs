c71a4ca4316b96a24b29a5a4862f285cd2d29ac2
Shane Panter <shanepanter@boisestate.edu>
2023-09-22 21:06:57 -0600

    Basic working with no paging

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import {createRequire} from 'node:module'
-import { cloneAll } from "../src/ghclass.mjs";
+import { Command } from 'commander';
+import { createRequire } from 'node:module';
+import { cloneAll } from '../src/ghclass.mjs';
 
-const require = createRequire(import.meta.url)
-const pkg = require('../package.json')
+const require = createRequire(import.meta.url);
+const pkg = require('../package.json');
 
 const program = new Command();
 
 program
-    .name("ghc")
-    .description("CLI downloader for github repositories created with classrooms.github.com")
-    .version(pkg.version)
-    .requiredOption("-r, --roster <name>","File name of the class roster (ex. class_roster.csv)")
-    .requiredOption("-o, --organization <name>", "Name of your Github organization")
-    .requiredOption("-a, --assignment <name> ","Name of the assignment");
+  .name('ghc')
+  .description('CLI downloader for github repositories created with classrooms.github.com')
+  .version(pkg.version)
+  .requiredOption('-r, --roster <name>', 'File name of the class roster (ex. class_roster.csv)')
+  .requiredOption('-o, --organization <name>', 'Name of your Github organization')
+  .requiredOption('-a, --assignment <name> ', 'Name of the assignment');
 
 program.parse(process.argv);
 const options = program.opts();
 
 await cloneAll(options.organization, options.assignment, options.roster);
-console.log("Done!");
+console.log('Done!');
--- src/ghclass.mjs
+++ src/ghclass.mjs
@@ -1,41 +1,47 @@
-import { parse } from 'csv-parse/sync'
-import fs from 'node:fs/promises'
+import { parse } from 'csv-parse/sync';
+import fs from 'node:fs/promises';
+import { exec } from 'node:child_process';
+import util from 'node:util';
+
+const execp = util.promisify(exec);
 
 /**
  * Returns a GitHub url using the ssh protocol
  * @param {object} student A populated student object
  * @param {string} organization A valid GitHub organization
  * @param {string} project A project identifier
- * @returns A string formatted to clone a github repo over SSH
+ * @return {string} A string formatted to clone a github repo over SSH
  */
-export function gitHubRepoUrlSSH (student, organization, project) {
-    return "git@github.com:" + organization + "/" +
-        project + "-" + (student.team || student.githubUserName);
+export function gitHubRepoUrlSSH(student, organization, project) {
+  return `git@github.com:${
+    organization}/${project}-${
+    student.team || student.githubUserName
+  } ${student.team || student.email}`;
 }
 
 /**
  * Loads a roster that was exported from classrooms.github.com
  * and returns an array of student objects
  * @param {string} path Path to file
- * @returns An array containing the all the students
+ * @return {Array} An array containing the all the students
  */
 export async function loadRosterCSV(path) {
-    const fle = await fs.readFile(path, { encoding: 'utf-8' });
-    const csv = parse(fle);
-    let rval = [];
-    //Groups can have spaces in them and need to be replaced with -
-    //to match the format that classrooms is using
-    for (let student of csv.slice(1)) {
-        let tmp = {
-            email: student[0],
-            githubUserName: student[1],
-            githubID: student[2],
-            name: student[3],
-            team: student.length === 5 ? student[4].replaceAll(' ', '-') : null
-        }
-        rval.push(tmp);
-    }
-    return rval;
+  const fle = await fs.readFile(path, { encoding: 'utf-8' });
+  const csv = parse(fle);
+  const rval = [];
+  // Groups can have spaces in them and need to be replaced with -
+  // to match the format that classrooms is using
+  csv.slice(1).forEach((student) => {
+    const tmp = {
+      email: student[0],
+      githubUserName: student[1],
+      githubID: student[2],
+      name: student[3],
+      team: student.length === 5 ? student[4].replaceAll(' ', '-') : null,
+    };
+    rval.push(tmp);
+  });
+  return rval;
 }
 
 /**
@@ -49,9 +55,25 @@ export async function loadRosterCSV(path) {
  * @param {string} roster The path to the class roster
  */
 export async function cloneAll(org, project, roster) {
-    console.log("Clone with options: " + org + " " + project + " " + roster);
-    const students = await loadRosterCSV(roster);
-    for (s of students){
-        console.log("Cloning: " + gitHubRepoUrlSSH(s,org,project));
+  console.log(`Clone with options: ${org} ${project} ${roster}`);
+  const students = await loadRosterCSV(roster);
+  const clones = [];
+  students.forEach((s) => {
+    if (s.githubID !== '') {
+      const r = gitHubRepoUrlSSH(s, org, project);
+      clones.push(execp(`git clone ${r}`).catch(() => {
+        console.log(`-->student: ${s.email} has not accepted the assignment`);
+      }));
+    } else {
+      console.log(`student: ${s.email} has not accepted the assignment`);
     }
+  });
+
+  await Promise.allSettled(clones).then((results) => {
+    results.forEach((result) => {
+      if (result.value) {
+        console.log(result.value.stderr.trim());
+      }
+    });
+  });
 }
